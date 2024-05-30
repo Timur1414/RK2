3684de62c9e4d6d583bf5f4cfabf7509b1b5bd22
Тимур <63254053+Timur1414@users.noreply.github.com>
2024-05-30 10:19:14 +0300

    Update prototype.cpp

--- prototype.cpp
+++ prototype.cpp
@@ -6,34 +6,8 @@
  ************************************************************************/
 
 #include<iostream>
-using namespace std;
-class Prototype{
-public:
-	virtual ~Prototype(){
+#include <prototype.h>
 
-	}
-	virtual Prototype* Clone() const = 0;
-protected:
-	Prototype(){
-
-	}
-};
-class ConcretePrototype:public Prototype{
-public:
-	ConcretePrototype(){
-
-	}
-	ConcretePrototype(const ConcretePrototype& cp){
-		*this = cp;	
-		cout<<"ConcretePrototype copy ..."<<endl;
-	}
-	~ConcretePrototype(){
-
-	}
-	virtual Prototype* Clone() const{
-		return new ConcretePrototype(*this);
-	}
-};
 int main(){
 	Prototype* p = new ConcretePrototype();
 	Prototype* p1 = p->Clone();
