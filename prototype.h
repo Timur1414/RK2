966c351b901834c2d6814ef31821ac0722a8480c
Тимур <63254053+Timur1414@users.noreply.github.com>
2024-05-30 10:18:35 +0300

    Update prototype.h

--- prototype.h
+++ prototype.h
@@ -1,1 +1,30 @@
+#pragma once
 
+using namespace std;
+class Prototype{
+public:
+	virtual ~Prototype(){
+
+	}
+	virtual Prototype* Clone() const = 0;
+protected:
+	Prototype(){
+
+	}
+};
+class ConcretePrototype:public Prototype{
+public:
+	ConcretePrototype(){
+
+	}
+	ConcretePrototype(const ConcretePrototype& cp){
+		*this = cp;	
+		cout<<"ConcretePrototype copy ..."<<endl;
+	}
+	~ConcretePrototype(){
+
+	}
+	virtual Prototype* Clone() const{
+		return new ConcretePrototype(*this);
+	}
+};
